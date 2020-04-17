bd5523727536baff974955b281a8650d992475f3
wanghuizhou <wanghuizhou@guazi.com>
2020-04-17 17:12:01 +0800

    欲穷千里目

--- AlgorithmNotice/AlgorithmNotice/AppDelegate.m
+++ AlgorithmNotice/AlgorithmNotice/AppDelegate.m
@@ -23,6 +23,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // 白日依山尽
     // 黄河入海流
+    // 欲穷千里目
 //    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 //    [self.window setBackgroundColor:[UIColor whiteColor]];
 //    GuideViewController * guideVC = [[GuideViewController alloc] init];
