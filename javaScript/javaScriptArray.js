6266c3bd8859c8a711e20fc5a2b16ef95da57365
wanghuizhou <wanghuizhou@guazi.com>
2020-08-24 14:24:15 +0800

    [comments]6.一周中的第几天一周中的第几天 [jira_id] [version] 0824

--- javaScript/javaScriptArray.js
+++ javaScript/javaScriptArray.js
@@ -349,3 +349,51 @@ const intersect = function (nums1, nums2) {
     }
     return result;
 }
+
+/*************************6.一周中的第几天一周中的第几天*********************************************/
+/****
+ * 
+ * 给你一个日期，请你设计一个算法来判断它是对应一周中的哪一天。 输入为三个整数:day、month 和 year，分别表示日、月、年。
+ * 您返回的结果必须是这几个值中的一个 {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}。
+ * 说明:给出的日期一定是在 1971 到 2100 年之间的有效日期。 
+ * 示例 1:
+ * 输入:day = 31, month = 8, year = 2019
+ * 输出:"Saturday"
+ * 示例 2:
+ * 输入:day = 18, month = 7, year = 1999 
+ * 输出:"Sunday"
+ * 示例 3:
+ * 输入:day = 15, month = 8, year = 1993
+ * 输出:"Sunday"
+ */
+const dayOfTheWeek = function (day, monthy, year) {
+    const date = new Date(Date.parse('${year}/${month}/${day}'));
+    const Week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    return Week[date.getDay()];
+}
+
+const dayOfTheWeekOther = function (dat, month, year) {
+    const Month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const Week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    // 1970年12月31日为星期四，即初始值为4
+    let count = 4;
+    // 算上此年前每年的日期，都先当365天算
+    count += (year - 1970 - 1) * 365;
+    // 算上此月前每个月的日期
+    for (leti = 1; i < month; i++) {
+        count += Month[i - 1];
+    }
+    // 算上此月的日期
+    count += day;
+    // 加上今年之前的闰年天数 for(lety=1971;y<=year-1;y++){
+    if ((y % 4 === 0 && y % 100 !== 0) || y % 400 === 0) {
+        count++;
+    }
+
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        if (month > 2) {
+            count++;
+        }
+    }
+    return Week[count % 7];
+}
