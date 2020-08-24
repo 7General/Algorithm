b45066268c7b4878f0022993898dd0da2202ad60
wanghuizhou <wanghuizhou@guazi.com>
2020-08-24 14:42:26 +0800

    [comments]7.存在重复元素 [jira_id] [version] 0824

--- javaScript/javaScriptArray.js
+++ javaScript/javaScriptArray.js
@@ -26,6 +26,9 @@ window.onload = function () {
     moveZeroes(moveZ)
     console.log(moveZ)
 
+    var dupArry = [1,1,1,3,3,4,3,2,4,2];
+    console.log(containsDuplicate(dupArry))
+
 }
 
 /*************************1.旋转数组*********************************************/
@@ -397,3 +400,35 @@ const dayOfTheWeekOther = function (dat, month, year) {
     }
     return Week[count % 7];
 }
+/*************************7.存在重复元素*********************************************/
+/***
+ * 
+ * 给定一个整数数组，判断是否存在重复元素。
+ * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回
+ * false。 
+ * 示例1
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ */
+const containsDuplicate = function(nums){
+    let index = 0;
+    for (let i = 1; i < nums.length; i++) {
+        let left =  nums[i];
+        let right = nums[index];
+        if(left === right){
+            index++;
+        }
+    }
+    if (index > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
