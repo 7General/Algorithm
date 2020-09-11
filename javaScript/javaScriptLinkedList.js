9ef3c8f472db398c0f70af0bdc3c0d1765f50d64
wanghuizhou <wanghuizhou@guazi.com>
2020-09-11 14:36:45 +0800

    [comments]5.删除链表的倒数第N个节点 [jira_id] [version]0911

--- javaScript/javaScriptLinkedList.js
+++ javaScript/javaScriptLinkedList.js
@@ -159,3 +159,26 @@ const reverseListOther = function(head) {
    head.next = null;
    return newHead;
 }
+/*************************5.删除链表的倒数第N个节点*********************************************/
+/***
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * 
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ */
+const removeNthFromEnd = function(head,n) {
+   let first = head;
+   let second = head;
+   while(n > 0){
+      first = first.next;
+      n--;
+   }
+   if(!first) return head.next;
+   // 当双向指针指向【3，4】的时候，则second=3。则sec.next = sec.next.next
+   while(first.next){
+      first = first.next;
+      second = second.next;
+   }
+   second.next = second.next.next;
+   return head;
+}
