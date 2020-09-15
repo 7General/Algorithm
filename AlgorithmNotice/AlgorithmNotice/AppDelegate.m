f1cdcc5542b5c08828af612da17380726554330b
wanghuizhou <wanghuizhou@guazi.com>
2020-09-15 14:05:20 +0800

    [comments]4,5,6 [jira_id] [version]0915

--- AlgorithmNotice/AlgorithmNotice/AppDelegate.m
+++ AlgorithmNotice/AlgorithmNotice/AppDelegate.m
@@ -68,6 +68,9 @@
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+    // 4
+    // 5
+    // 6
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
